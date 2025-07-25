347dc4e151ad0834ebf531ecce53e5dda31634b1
KeerthiKrish8 <keerthikak0393@gmail.com>
2025-07-25 10:22:40 +0530

    add image file

--- Sumanfrontend/src/App.jsx
+++ Sumanfrontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Product from '../src/Components/Product/Product';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
-         
+         <Route path="/product" element={<Product />} />
 
 
       </Routes>
